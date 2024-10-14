926e741647e006f48ff379a58b293163dcab99c3
robertoamato <robertoamatoz@gmail.com>
2024-10-14 12:56:12 -0300

    update

--- src/main/java/com/example/First_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/example/First_spring_app/controller/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.example.First_spring_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @Controller
 @ResponseBody
 @RestController
+@RequestMapping("/hello-world")
 public class HelloWorldController {
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+
+    }
+
 }
