1bf6b34f94db2c86ef74bb2d81937b84329e4a10
robertoamato <robertoamatoz@gmail.com>
2024-10-14 12:49:18 -0300

    Create HelloWorldController.java

---
+++ src/main/java/com/example/First_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.example.First_spring_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@Controller
+@ResponseBody
+@RestController
+public class HelloWorldController {
+}
